345c404925f17b1b45433b81627845f1dc7c4318
Peter Weidenbach <weidenba@cs.uni-bonn.de>
2017-08-31 07:40:55 +0200

    dependencies fixed

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ setup(
     packages=find_packages('src', exclude=['tests']),
     install_requires=[
         'entropy >= 0.9',
+        'common_helper_files >= 0.2'
+    ],
+    dependency_links=[
+        'https://github.com/fkie-cad/common_helper_files/tarball/master#egg=common_helper_files-0.2'
     ],
     author="Fraunhofer FKIE",
     author_email="peter.weidenbach@fkie.fraunhofer.de",
