2411268157a39111c8bfdb57c5956922872a31d8
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-08-16 16:28:25 +0200

    changed avg_entropy function so that is also accepts files
    as Path or file pointer

--- src/common_helper_unpacking_classifier/average_entropy.py
+++ src/common_helper_unpacking_classifier/average_entropy.py
@@ -1,40 +1,76 @@
+from __future__ import annotations
+
 import logging
-import sys
-from typing import Callable
+from io import BufferedReader
+from pathlib import Path
+from typing import Callable, Iterable
 
 from entropython import metric_entropy, shannon_entropy
-
 
 BLOCKSIZE = 4096
 
 
-def avg_entropy(input_data: bytes, block_size: int = BLOCKSIZE, entropy_function: Callable = metric_entropy) -> float:
-    '''
+def avg_entropy(
+    input_data: bytes | Path | BufferedReader,
+    block_size: int = BLOCKSIZE,
+    entropy_function: Callable = metric_entropy,
+) -> float:
+    """
     Calculates the average entropy of input_data regarding block_size
 
     :param input_data: input data
     :param block_size: shannon block size in bytes
     :param entropy_function: the function to use for entropy calculation
     :return: float
-    '''
-    offset = 0
+    """
     entropy_sum = 0
     number_of_blocks = 0
-    while offset < len(input_data):
-        current_block = input_data[offset:offset + block_size]
-        entropy_sum += entropy_function(current_block) * (len(current_block) / block_size)
-        offset += block_size
-        number_of_blocks += (len(current_block) / block_size)
+    for block in _iter_content_in_chunks(input_data, block_size):
+        block_weight = len(block) / block_size
+        entropy_sum += entropy_function(block) * block_weight
+        number_of_blocks += block_weight
     try:
         return entropy_sum / number_of_blocks
+    except ZeroDivisionError:
+        return 0.0
     except Exception as err:
-        logging.warning('Could not calculate entropy: {} {}'.format(sys.exc_info()[0].__name__, err))
+        logging.exception(f"Could not calculate entropy: {err}")
         return 0.0
 
 
-def avg_shannon_entropy(input_data, block_size=BLOCKSIZE):
-    return avg_entropy(input_data, block_size=block_size, entropy_function=metric_entropy)
+def _iter_content_in_chunks(
+    input_data: Path | bytes | BufferedReader,
+    chunk_size: int,
+) -> Iterable[bytes]:
+    if isinstance(input_data, Path):
+        with input_data.open("rb") as fp:
+            while chunk := fp.read(chunk_size):
+                yield chunk
+    elif isinstance(input_data, BufferedReader):
+        while chunk := input_data.read(chunk_size):
+            yield chunk
+    else:
+        for offset in range(0, len(input_data), chunk_size):
+            yield input_data[offset : offset + chunk_size]
 
 
-def avg_shannon_entropy_byte(input_data, block_size=BLOCKSIZE):
-    return avg_entropy(input_data, block_size=block_size, entropy_function=shannon_entropy)
+def avg_shannon_entropy(
+    input_data: bytes | Path | BufferedReader,
+    block_size: int = BLOCKSIZE,
+) -> float:
+    return avg_entropy(
+        input_data,
+        block_size=block_size,
+        entropy_function=metric_entropy,
+    )
+
+
+def avg_shannon_entropy_byte(
+    input_data: bytes | Path | BufferedReader,
+    block_size: int = BLOCKSIZE,
+) -> float:
+    return avg_entropy(
+        input_data,
+        block_size=block_size,
+        entropy_function=shannon_entropy,
+    )
