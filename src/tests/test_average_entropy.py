e5f6182f457ffdc7f53493fc1863bdf99d57b084
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-08-16 16:28:25 +0200

    changed avg_entropy function so that is also accepts files
    as Path or file pointer

--- src/tests/test_average_entropy.py
+++ src/tests/test_average_entropy.py
@@ -1,15 +1,33 @@
-import unittest
+from pathlib import Path
+
+import pytest
+import math
+
 from common_helper_unpacking_classifier.average_entropy import avg_entropy
 
+test_data_dir = Path(__file__).parent / "data"
 
-class TestAverageEntropy(unittest.TestCase):
 
-    def test_avg_entropy(self):
-        zero_entropy_data = 100 * b'\x00'
-        self.assertEqual(avg_entropy(zero_entropy_data), 0)
-        uncompressed_data = b'this is not compressed'
-        self.assertGreater(avg_entropy(uncompressed_data), 0.1)
-        self.assertGreater(0.9, avg_entropy(uncompressed_data))
+def test_avg_entropy():
+    zero_entropy_data = 100 * b"\x00"
+    assert math.isclose(avg_entropy(zero_entropy_data), 0.0)
+    uncompressed_data = b"this is not compressed"
+    assert 0.9 > avg_entropy(uncompressed_data) > 0.1
 
-    def test_avg_entropy_error_cases(self):
-        self.assertEqual(avg_entropy(b''), 0)
+
+def test_avg_entropy_error_cases():
+    assert math.isclose(avg_entropy(b""), 0.0)
+
+
+@pytest.mark.parametrize(
+    ("input_file_name", "expected_output"),
+    [
+        ("0_entropy", 0.0),
+        ("max_entropy", 1.0),
+    ],
+)
+def test_avg_entropy_from_file(input_file_name, expected_output):
+    zero_entropy_file = test_data_dir / input_file_name
+    assert avg_entropy(zero_entropy_file) == expected_output
+    with open(zero_entropy_file, "rb") as fp:
+        assert math.isclose(avg_entropy(fp), expected_output)
