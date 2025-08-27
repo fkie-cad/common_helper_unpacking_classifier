d855f8ee7c6ff6be29ce14373097f0bf542d5d10
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-08-27 10:13:44 +0200

    refactor: format get_size with ruff

--- src/common_helper_unpacking_classifier/get_size.py
+++ src/common_helper_unpacking_classifier/get_size.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 import logging
 import os
 import sys
+from pathlib import Path
 
 from common_helper_files import get_binary_from_file
 from entropython import metric_entropy as entropy
@@ -11,46 +14,49 @@ PADDING_ENTROPY_THRESHOLD = 0.1
 
 
 def get_file_size(file_path: str) -> int:
-    '''
+    """
     Returns the size of file in bytes. Returns zero on error.
 
     :param file_path: path to file
     :return: the size of file as number of bytes
-    '''
+    """
     try:
         return os.path.getsize(file_path)
     except Exception as err:
-        logging.error('Could not get file size: {} {}'.format(sys.exc_info()[0].__name__, err))
+        logging.error(f"Could not get file size: {sys.exc_info()[0].__name__} {err}")
         return 0
 
 
-def get_file_size_without_padding(file_path: str) -> int:
-    '''
+def get_file_size_without_padding(file_path: str | Path) -> int:
+    """
     Returns the size of file in bytes minus size of padding areas.
 
     :param file_path: path to file
     :type file_path: str
     :return: int
-    '''
+    """
     file_data = get_binary_from_file(file_path)
     return get_binary_size_without_padding(file_data)
 
 
-def get_binary_size_without_padding(data: bytes, blocksize: int = BLOCKSIZE,
-                                    padding_entropy_threshold: float = PADDING_ENTROPY_THRESHOLD) -> int:
-    '''
+def get_binary_size_without_padding(
+    data: bytes,
+    blocksize: int = BLOCKSIZE,
+    padding_entropy_threshold: float = PADDING_ENTROPY_THRESHOLD,
+) -> int:
+    """
     Returns the size of input_data in bytes minus size of padding areas.
 
     :param data: input data
     :param blocksize: block-size regarding padding detection
     :param padding_entropy_threshold: shannon entropy threshold regarding padding detection
     :return: the size of input_data without padding
-    '''
+    """
     original_size = len(data)
     padding_size = 0
     offset = 0
     while offset < original_size:
-        if entropy(data[offset:offset + blocksize]) <= padding_entropy_threshold:
+        if entropy(data[offset : offset + blocksize]) <= padding_entropy_threshold:
             padding_size += blocksize
         offset += blocksize
     return original_size - padding_size
