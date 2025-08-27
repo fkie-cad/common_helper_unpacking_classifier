7dcb9a95f3072bd910bea28b37a5c32b5ab8204c
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-08-27 10:29:58 +0200

    feat: also use chunk iteration on get_size methods
    to improve memory efficiency

--- src/tests/test_get_size.py
+++ src/tests/test_get_size.py
@@ -1,18 +1,9 @@
-import unittest
 import os
-from common_helper_unpacking_classifier.get_size import get_file_size, get_binary_size_without_padding
+from common_helper_unpacking_classifier.get_size import get_binary_size_without_padding
 
 
-class TestGetSize(unittest.TestCase):
-
-    def test_get_file_size(self):
-        none_existing_file = '/foo/nonexisting'
-        self.assertEqual(get_file_size(none_existing_file), 0)
-        existing_file = os.path.abspath(__file__)
-        self.assertGreater(get_file_size(existing_file), 1)
-
-    def test_get_bin_size_without_padding(self):
-        no_padding = b'abcdefgt'
-        assert get_binary_size_without_padding(no_padding) == len(no_padding)
-        with_padding = os.urandom(4096) + b'\x00' * 1024 + os.urandom(4096)
-        assert get_binary_size_without_padding(with_padding) == 4096*2
+def test_get_bin_size_without_padding():
+    no_padding = b'abcdefgt'
+    assert get_binary_size_without_padding(no_padding) == len(no_padding)
+    with_padding = os.urandom(4096) + b'\x00' * 1024 + os.urandom(4096)
+    assert get_binary_size_without_padding(with_padding) == 4096*2
